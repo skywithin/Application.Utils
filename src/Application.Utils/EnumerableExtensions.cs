9fcb57023f541dcfbb5af7a2c2144babdd1158d2
Max Vagner <sydneymax@gmail.com>
2020-06-27 13:36:08 +1000

    Add EnumerableExtensions.DistinctBy

---
+++ src/Application.Utils/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application.Utils
+{
+    public static class EnumerableExtensions
+    {
+        //Thanks https://stackoverflow.com/questions/489258/linqs-distinct-on-a-particular-property
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> src, Func<TSource, TKey> func)
+        {
+            HashSet<TKey> keys = new HashSet<TKey>();
+
+            foreach (TSource elem in src)
+            {
+                if (keys.Add(func(elem)))
+                {
+                    yield return elem;
+                }
+            }
+        }
+    }
+}
